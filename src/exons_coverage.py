8d64080ec4296219cb4ce98a22550ae25a5516e4
nock <ladrek13@mail.ru>
2024-10-14 14:22:17 +0700

    multiprocessing to get exons coverage

--- src/exons_coverage.py
+++ src/exons_coverage.py
@@ -5,23 +5,60 @@ from utils import get_value_from_dict
 from tqdm import tqdm
 from colorama import Fore, Back, Style
 from LINE_preparation import LINE_Processing
+import time
+from concurrent.futures import ProcessPoolExecutor
 
 class ExonCoverage:
 
-    def __init__(self, df: pd.DataFrame, bamfile: str) -> None:
+    def __init__(self, df: pd.DataFrame, bamfile: str, cores: int = 1) -> None:
         self.df = df
         self.bamfile = bamfile
         self.line_processing = LINE_Processing()
+        self.cores = cores
+
 
     def coverage(self):
         LINE_genes = self.line_processing.filter_by_LINE_genes()
         only_exons_df = self.df.loc[(self.df['name'].str.contains('exon'))]
         filtered_df = only_exons_df.loc[(self.df['gene'].isin(LINE_genes))]
-        filtered_df = filtered_df[:10]
+        # filtered_df = filtered_df[:1001]
+
+        dfs = self.split_df_to_cores(filtered_df)
+        start_time = time.time()
         print(Fore.CYAN + Style.BRIGHT + "Получение покрытия у экзонов")
-        filtered_df.loc[:, 'coverage'] = [self.get_coverage_to_exon(row) for row in tqdm(filtered_df.itertuples(), total=len(filtered_df))]
+        
+
+        with ProcessPoolExecutor(max_workers=4) as executor:
+            results = list(executor.map(self.multiple_coverage, dfs))            
+
+        df_with_coverage = pd.concat(results)          
+
         print(Style.RESET_ALL)
+        end_time = time.time()
+        total_time = end_time - start_time
+        print(f'Итоговое время выполнения программы: {total_time:.2f} секунд')        
+        return df_with_coverage
+
+    def split_df_to_cores(self, filtered_df: pd.DataFrame) -> list[pd.DataFrame]:
+        len_df = len(filtered_df)
+        len_one_df = len_df // self.cores
+        dfs = []
+        x = 0
+        for i in range(self.cores):
+            start_pos = x
+            end_pos = x + len_one_df
+            dfs.append(filtered_df[start_pos:end_pos])
+            x += len_one_df
+        
+        if x < len_df:
+            dfs.append(filtered_df[x:len_df])
+        
+        return dfs
+
+    def multiple_coverage(self, filtered_df):
+        filtered_df.loc[:, 'coverage'] = [self.get_coverage_to_exon(row) for row in tqdm(filtered_df.itertuples(), total=len(filtered_df))]
         return filtered_df
+
         
     def get_coverage_to_exon(self, exon):
         exon = exon._asdict()
@@ -68,12 +105,12 @@ class CoverageManager:
         df = pd.read_csv('../genes/exons_and_intron_positions_in_genome.csv', sep='\t')
         if not args.bamfile or not os.path.exists(args.bamfile):
             raise ValueError("BAM file does not exist or is not specified.")
-        exon_coverage = ExonCoverage(df=df, bamfile = args.bamfile)
+        exon_coverage = ExonCoverage(df=df, bamfile = args.bamfile, cores = args.cores)
         filtered_df = exon_coverage.coverage()
         filtered_df.to_csv(args.output, sep='\t', index=False) 
 
 if __name__ == "__main__":
     df = pd.read_csv('../genes/exons_and_intron_positions_in_genome.csv', sep='\t')
-    exon_coverage = ExonCoverage(df=df)
+    exon_coverage = ExonCoverage(df=df, bamfile='../../alter_LINE_expr/SRR14374304_sorted.bam', cores=4)
     filtered_df = exon_coverage.coverage()
     filtered_df.to_csv('SRX_exons_coverage.csv', sep='\t', index=False)
--- src/main.py
+++ src/main.py
@@ -26,17 +26,20 @@ def main():
     coverage = subparsers.add_parser('coverage', help='Получение покрытия для списка экзонов')
     coverage.add_argument('-o', '--output', help='Название для итогового файла с покрытием')
     coverage.add_argument("-b", "--bamfile", help="Bam file to get coverage", required=True)
+    coverage.add_argument('-c', '--cores', type=int, default=4, help='Количество ядер для использования')
     coverage.set_defaults(func=manager.coverage)
 
-    coverage = subparsers.add_parser('cluster', help='Кластеризация покрытий экзонов относительно LINE')
-    coverage.add_argument('-i', '--input', help='Название для итогового файла с покрытием', required=True)
-    coverage.add_argument('-o', '--output', help='Название для итогового файла с кластеризованным покрытием', required=True)
-    coverage.set_defaults(func=manager.cluster)
+    coverage_cluster = subparsers.add_parser('cluster', help='Кластеризация покрытий экзонов относительно LINE')
+    coverage_cluster.add_argument('-i', '--input', help='Название для итогового файла с покрытием', required=True)
+    coverage_cluster.add_argument('-o', '--output', help='Название для итогового файла с кластеризованным покрытием', required=True)
+    coverage_cluster.add_argument('-c', '--cores', type=int, default=4, help='Количество ядер для использования')
+    coverage_cluster.set_defaults(func=manager.cluster)
 
     coverage_seed = subparsers.add_parser('coverageseed', help='Получение покрытия затравок LINE')
     coverage_seed.add_argument('-i', '--input', help='Название для итогового файла с покрытием', required=True)
     coverage_seed.add_argument('-o', '--output', help='Название для итогового файла с кластеризованным покрытием', required=True)
     coverage_seed.add_argument("-b", "--bamfile", help="Bam file to get coverage", required=True)
+    coverage_seed.add_argument("-c", "--cores", help="Кол-во ядер для расчета покрытия", required=False, default=1)
     coverage_seed.set_defaults(func=manager.coverage_seed)
 
 
